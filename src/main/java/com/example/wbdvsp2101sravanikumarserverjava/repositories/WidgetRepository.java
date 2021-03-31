4cd155bdb44dd81d52899a7e0b259cb0e78725ee
sravanikumar <kumar.sra@northeastern.edu>
2021-03-31 00:36:43 -0400

    created widget repository

---
+++ src/main/java/com/example/wbdvsp2101sravanikumarserverjava/repositories/WidgetRepository.java
@@ -0,0 +1,8 @@
+package com.example.wbdvsp2101sravanikumarserverjava.repositories;
+
+import com.example.wbdvsp2101sravanikumarserverjava.models.Widget;
+import org.springframework.data.repository.CrudRepository;
+
+public interface WidgetRepository extends CrudRepository<Widget, Long> {
+
+}
